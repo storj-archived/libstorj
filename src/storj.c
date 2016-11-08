70eae74a75c988ae4b53fb48b17871061875dcf0
Braydon Fuller <courier@braydon.com>
2016-11-08 11:25:07 -0500

    Initial def of api methods

--- src/storj.c
+++ src/storj.c
@@ -41,3 +41,83 @@ struct json_object* storj_bridge_get_info()
 
     return obj;
 }
+
+struct json_object* storj_bridge_get_buckets()
+{
+
+}
+
+struct json_object* storj_bridge_create_bucket()
+{
+
+}
+
+struct json_object* storj_bridge_delete_bucket()
+{
+
+}
+
+struct json_object* storj_bridge_list_files()
+{
+
+}
+
+struct json_object* storj_bridge_create_bucket_token()
+{
+
+}
+
+struct json_object* storj_bridge_delete_file()
+{
+
+}
+
+struct json_object* storj_bridge_create_frame()
+{
+
+}
+
+struct json_object* storj_bridge_get_frames()
+{
+
+}
+
+struct json_object* storj_bridge_get_file_info()
+{
+
+}
+
+struct json_object* storj_bridge_get_frame()
+{
+
+}
+
+struct json_object* storj_bridge_delete_frame()
+{
+
+}
+
+struct json_object* storj_bridge_add_shard_to_frame()
+{
+
+}
+
+struct json_object* storj_bridge_replicate_file()
+{
+
+}
+
+struct json_object* storj_bridge_store_file()
+{
+
+}
+
+struct json_object* storj_bridge_get_file_pointers()
+{
+
+}
+
+struct json_object* storj_bridge_resolve_file()
+{
+
+}
