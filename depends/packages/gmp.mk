8d11e48c3e5c373033906cc791e823fa60209f7f
Braydon Fuller <courier@braydon.com>
2017-02-02 19:07:19 -0500

    Check hash of downloads

--- depends/packages/gmp.mk
+++ depends/packages/gmp.mk
@@ -1,5 +1,5 @@
 package=gmp
 $(package)_version=6.1.2
 $(package)_download_path=https://ftp.gnu.org/gnu/gmp/gmp-$($(package)_version).tar.bz2
-$(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=87b565e89a9a684fe4ebeeddb8399dce2599f9c9049854ca8c0dfbdea0e21912
+$(package)_file_name=$(package)-$($(package)_version).tar.bz2
+$(package)_sha256_hash=5275bb04f4863a13516b2f39392ac5e272f5e1bb8057b18aec1c9b79d73d8fb2
