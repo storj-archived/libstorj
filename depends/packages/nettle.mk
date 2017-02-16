8472369bbb7f63996428168858d2ac04d965fd1a
Braydon Fuller <courier@braydon.com>
2017-02-15 19:45:04 -0500

    Build depends with x86_64-pc-linux-gnu
    Changes still need to be made in a way that will not affect build with x86_64-w64-mingw32

--- depends/packages/nettle.mk
+++ depends/packages/nettle.mk
@@ -3,5 +3,5 @@ $(package)_version=3.3
 $(package)_download_path=https://ftp.gnu.org/gnu/nettle/nettle-$($(package)_version).tar.gz
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=46942627d5d0ca11720fec18d81fc38f7ef837ea4197c1f630e71ce0d470b11e
-$(package)_config_env=PKG_CONFIG_LIBDIR="$(PREFIX_DIR)/lib/pkgconfig" CFLAGS="-I$(PREFIX_DIR)include -L$(PREFIX_DIR)lib -static"
+$(package)_config_env=PKG_CONFIG_LIBDIR="$(PREFIX_DIR)/lib/pkgconfig" CFLAGS="-I$(PREFIX_DIR)include -L$(PREFIX_DIR)lib"
 $(package)_config_opts=
