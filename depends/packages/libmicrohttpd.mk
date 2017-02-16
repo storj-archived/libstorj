4ab3a44c907faaeecdf184eb157b2c8bce3db311
Alexander Leitner <leitnersalex@gmail.com>
2017-02-16 13:10:28 -0500

    Build on mac for `x86_64-apple-darwin11`

--- depends/packages/libmicrohttpd.mk
+++ depends/packages/libmicrohttpd.mk
@@ -1,7 +1,7 @@
 package=libmicrohttpd
-$(package)_version=0.9.52
+$(package)_version=0.9.51
 $(package)_download_path=https://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-$($(package)_version).tar.gz
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=54797f6e763d417627f89f60e4ae0a431dab0523f92f83def23ea02d0defafea
+$(package)_sha256_hash=3000bb0ba219061f2ea60251b8ea3d5f694e8c6ce1706bbff57e7e24867a23c7
 $(package)_config_env=
 $(package)_config_opts=
