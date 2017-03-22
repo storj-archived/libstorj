76ff6cabda9597e565b3fe6411d68ef300cb8f3a
Braydon Fuller <braydon@storj.io>
2017-03-22 13:51:56 -0400

    Do not use threaded resolver for windows

--- depends/packages/libcurl.mk
+++ depends/packages/libcurl.mk
@@ -10,8 +10,11 @@ $(package)_config_opts_default=--disable-ftp --disable-file --disable-ldap --dis
 
 # mingw specific settings
 $(package)_config_env_mingw32=LIBS="-lcrypt32 -lnettle -lhogweed -lgmp" LD_LIBRARY_PATH="$(PREFIX_DIR)lib" PKG_CONFIG_LIBDIR="$(PREFIX_DIR)lib/pkgconfig" CPPFLAGS="-I$(PREFIX_DIR)include -DCURL_STATIC_LIB -static" LDFLAGS="-L$(PREFIX_DIR)lib"
+$(package)_config_opts_mingw32=--disable-ftp --disable-file --disable-ldap --disable-ldaps --disable-rtsp --disable-dict --disable-telnet --disable-tftp --disable-pop3 --disable-imap --disable-smb --disable-smtp --disable-gopher --enable-proxy --without-ssl --with-gnutls="$(PREFIX_DIR)" --without-ca-path --without-ca-bundle --with-ca-fallback --disable-telnet
 $(package)_config_env_x86_64-w64-mingw32=$($(package)_config_env_mingw32)
 $(package)_config_env_i686-w64-mingw32=$($(package)_config_env_mingw32)
+$(package)_config_opts_x86_64-w64-mingw32=$($(package)_config_opts_mingw32)
+$(package)_config_opts_i686-w64-mingw32=$($(package)_config_opts_mingw32)
 
 # 32-bit linux specific settings
 $(package)_config_env_i686-pc-linux-gnu=LIBS="-lnettle -lhogweed -lgmp" LD_LIBRARY_PATH="$(PREFIX_DIR)lib" PKG_CONFIG_LIBDIR="$(PREFIX_DIR)lib/pkgconfig" CPPFLAGS="-I$(PREFIX_DIR)include -m32" LDFLAGS="-L$(PREFIX_DIR)lib -m32"
