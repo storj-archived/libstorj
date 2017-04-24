11aee63fd3adbb899124b2937d39695dcdef2a25
William Blankenship <william.jblankenship@gmail.com>
2017-04-24 17:52:47 -0400

    Compile RS implementation to js
    Works in Node.js and the browser (with a small hack)

---
+++ js/index.js
@@ -0,0 +1,43 @@
+Module = {
+  ENVIRONMENT: "NODE"
+}
+
+var rs = require('./rs.ec')
+
+var ReedSolomon = function ReedSolomon(dataShards, parityShards) {
+  dataShards = dataShards | 0
+  parityShards = parityShards | 0
+  if(!(this instanceof ReedSolomon)) {
+    return new ReedSolomon(dataShards, parityShards)
+  }
+  rs.ccall('fec_init', null)
+  this._rs = rs.ccall('reed_solomon_new',
+    'number',
+    ['number', 'number'],
+    [dataShards, parityShards])
+  this._ds = dataShards
+  this._ps = parityShards
+  return this
+}
+
+ReedSolomon.prototype.encode = function encode(data) {
+  var bs = data.length / this._ds
+  var result = new Uint8Array(this._ps * bs)
+  var err = rs.ccall('reed_solomon_encode',
+    'number',
+    ['array', 'array', 'number', 'number'],
+    [data, result, bs, data.length])
+  if (err !== 0) {
+    throw new Error(`Received error ${err} from libstorj`)
+  }
+  return result
+}
+
+ReedSolomon.prototype.cleanup = function cleanup() {
+  return rs.ccall('reed_solomon_release',
+    null,
+    ['number'],
+    [this._rs])
+}
+
+module.exports = ReedSolomon
