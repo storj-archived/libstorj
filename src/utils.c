25a5f989b9d30027a9c3fd0e1dd95c7db8b4c495
Braydon Fuller <braydon@storj.io>
2017-05-03 14:39:07 -0400

    Fix ARM build
    `'FALLOC_FL_ZERO_RANGE' undeclared`
    
    Zeroing isn't necessary, and should work without.

--- src/utils.c
+++ src/utils.c
@@ -253,9 +253,9 @@ win_finished:
 #elif HAVE_POSIX_FALLOCATE
     return posix_fallocate(fd, 0, length);
 #elif __unix__
-    return fallocate(fd, FALLOC_FL_ZERO_RANGE, 0, length);
+    return fallocate(fd, 0, 0, length);
 #elif __linux__
-    return fallocate(fd, FALLOC_FL_ZERO_RANGE, 0, length);
+    return fallocate(fd, 0, 0, length);
 #elif __APPLE__
     fstore_t store = {F_ALLOCATECONTIG, F_PEOFPOSMODE, 0, length, 0};
     // Try to get a continous chunk of disk space
