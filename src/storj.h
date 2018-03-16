ddc10b8f9735dddcdcb0816c0000640caebc49fe
kishore <kishore@storj.io>
2018-03-16 14:13:21 -0400

    Added the color codes used for debug messages

--- src/storj.h
+++ src/storj.h
@@ -45,8 +45,14 @@ extern "C" {
 #include <unistd.h>
 #endif
 
+#define KNRM  "\x1B[0m"
 #define KRED  "\x1B[31m"
 #define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
 #define RESET "\x1B[0m"
 
 // File transfer success
