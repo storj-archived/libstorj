cef7d32d1dd592f28e7a008f27c4f13e236f0042
Bryan White <bryanchriswhite@gmail.com>
2019-08-06 11:53:26 +0200

    add comments

--- sim-cli.sh
+++ sim-cli.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 set -ueo pipefail
 
+###
+# sim-cli.sh:
+#   + build libstorj cli at ./storj-cli if it doesn't exist
+#   + set environment variables for using development defaults
+#       and storj-sim satellite address
+#   + forwards all cli arguments to libstorj cli
+#
+#  (see: ./storj-cli --help)
+###
+
 export STORJ_BRIDGE=127.0.0.1:10000
 export STORJ_DEFAULTS=dev
 
--- tests.sh
+++ tests.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 set -ueo pipefail
 
+###
+# test.sh:
+#   + set environment variables for API key, storj-sim
+#       satellite address, and temp directory
+#   + build libstorj test binary (and dependencies, i.e. libuplink)
+#   + if built successfully, execute test binary
+#   + if passing, delete test binary
+#
+#  (see: ./tests.sh --help)
+###
+
 usage="usage: $0 <serialized API key>"
 
 if [[ $# -ne 1 ]]; then
